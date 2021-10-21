879650642b5e3420c5a77f9f89d0016fad64ac66
Glaydson Pacifico <glaydson.pacifico@gmail.com>
2021-10-21 20:54:36 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const linksSocialMedia = {
+  github: 'GlaydsonPacifico',
+  youtube: 'channel/UC2NZbQRWA5sntReROqlUStQ',
+  facebook: 'glaydson.miranda.3',
+  instagram: 'glaydson_',
+  twitter: 'GlaydsonP__'
+}
+
+function changeSocialMediaLinks90() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks90()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
