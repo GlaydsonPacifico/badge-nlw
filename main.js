95d9c8c02deeb3ce8cca1b04fa55bfbb0be922dd
Glaydson Pacifico <glaydson.pacifico@gmail.com>
2021-10-24 15:40:59 -0300

    correcting function name

--- main.js
+++ main.js
@@ -6,14 +6,14 @@ const linksSocialMedia = {
   twitter: 'GlaydsonP__'
 }
 
-function changeSocialMediaLinks90() {
+function changeSocialMediaLinks() {
   for (let li of socialLinks.children) {
     const social = li.getAttribute('class')
     li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
   }
 }
 
-changeSocialMediaLinks90()
+changeSocialMediaLinks()
 
 function getGitHubProfileInfos() {
   const url = `https://api.github.com/users/${linksSocialMedia.github}`
